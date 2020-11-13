dd5982737ea9db4f88defa7ecef76294ee4915a2
Justin <duckweiser@gmail.com>
2020-11-12 22:35:16 -0600

    cleaned up js file a bit

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -21,55 +21,6 @@ button.on("click", function() {
 
   console.log(filteredData);
 
-  //  array?
-  var datetime = filteredData.map(input1 => input1.datetime);
-  var state = filteredData.map(input1 => input1.state);
-
-
-  // console.log(datetime);
-  // console.log(filteredData.length);
-
-  // store values?
-  var city = filteredData.city;
-  var state = filteredData.state;
-  var country = filteredData.country;
-  var shape = filteredData.shape;
-  var duration = filteredData.duration;
-  var commento = filteredData.comments;
-  var dateo = filteredData.map(row => row[0]);
-
-  console.log(datetime)
-  console.log(state)
-
-// d3.json(filteredData).then(function(data) {
-//     var dateo = data.dataset.data.map(row => row[0]);
-//     var city = data.dataset.data.map(row => row[1]);
-//     var state = data.dataset.data.map(row => row[2]);
-//     var country = data.dataset.data.map(row => row[3]);
-//     var shape = data.dataset.data.map(row => row[4]);
-//     var duration = data.dataset.data.map(row => row[5]);
-//     var commento = data.dataset.data.map(row => row[6]);
-//     buildTable(dateo, city, state, country, shape, duration, commento);
-  // });
-
-  console.log(commento);
-
-  // remove any data from the table
-  // output.html("");
-
-// });
-
-// function buildTable(datetime, city, state, country, shape, duration,commento) {
-//     var table = d3.select("#ufo-table");
-//     var tbody = table.select("tbody");
-//     var trow;
-//     for (var i = 0; i < filteredData.length; i++) {
-//       trow = tbody.append("tr");
-//       trow.append("td").text(datetime[i]);
-//     }
-//   };
-
-
 
 // NEW STUFF //
 
